fc38750ea0e90b8afe10fbb9cc4619c747630882
Ivan Suslov <ioankingisepp@mail.ru>
2019-01-08 04:50:22 +0300

    Переработка алгоритма отрисовывания комментариев

--- js/preview.js
+++ js/preview.js
@@ -9,6 +9,7 @@
 
   var previewClass = {
     HIDDEN: 'hidden',
+    VISUALLY_HIDDEN: 'visually-hidden',
     CLICK_TRAGET: 'picture__img',
     LOADER: 'comments-loader',
     COMMENTS_FULL_COUNT: 'comments-count',
@@ -46,23 +47,22 @@
   var previewBlock = document.querySelector(previewSelector.BIG_PICTURE);
   var loader = document.querySelector(previewSelector.COMMENT_LOADER);
 
-  var socialCommentCount = previewBlock.querySelector(previewSelector.SOCIAL_COMMENT_COUNT);
-
-  /* setCommantCount text */
-  socialCommentCount.innerHTML = '';
-  var commentsCountCurrent = document.createElement(commentCountText.TAG);
-  commentsCountCurrent.classList.add(previewClass.COMMENTS_CURRENT_COUNT);
-  var commentsCountFull = document.createElement(commentCountText.TAG);
-  commentsCountFull.classList.add(previewClass.COMMENTS_FULL_COUNT);
-  var textFrom = document.createTextNode(commentCountText.FROM);
-  var textCommentaries = document.createTextNode(commentCountText.COMMENT);
-  socialCommentCount.appendChild(commentsCountCurrent);
-  socialCommentCount.appendChild(textFrom);
-  socialCommentCount.appendChild(commentsCountFull);
-  socialCommentCount.appendChild(textCommentaries);
-
-  var maxComment = 0;
-  var commentsData;
+  var renderCommentCount = function (countCurrent, countFull) {
+    var socialCommentCount = previewBlock.querySelector(previewSelector.SOCIAL_COMMENT_COUNT);
+    socialCommentCount.innerHTML = '';
+    var commentsCountCurrent = document.createElement(commentCountText.TAG);
+    commentsCountCurrent.classList.add(previewClass.COMMENTS_CURRENT_COUNT);
+    commentsCountCurrent.textContent = countCurrent;
+    var commentsCountFull = document.createElement(commentCountText.TAG);
+    commentsCountFull.classList.add(previewClass.COMMENTS_FULL_COUNT);
+    commentsCountFull.textContent = countFull;
+    var textFrom = document.createTextNode(commentCountText.FROM);
+    var textCommentaries = document.createTextNode(commentCountText.COMMENT);
+    socialCommentCount.appendChild(commentsCountCurrent);
+    socialCommentCount.appendChild(textFrom);
+    socialCommentCount.appendChild(commentsCountFull);
+    socialCommentCount.appendChild(textCommentaries);
+  };
 
   var onClosePreview = function () {
     previewBlock.removeEventListener('click', onUpdateComments);
@@ -72,9 +72,7 @@
   var getPreviewData = function (element) {
     var pictureId = element.getAttribute('data-id');
     window.util.openPopup(previewSelector.BIG_PICTURE, previewSelector.BODY);
-    commentsData = window.gallery.elementList[pictureId];
-    maxComment = Math.min(COMMENT_VIEW_MAX, commentsData.comments.length);
-    window.preview.setPreview();
+    window.preview.setPreview(window.gallery.elementList[pictureId]);
 
     closeButton.addEventListener('click', onClosePreview);
     previewBlock.addEventListener('click', onUpdateComments);
@@ -95,48 +93,59 @@
 
   var onUpdateComments = function (evt) {
     if (evt.target.classList.contains(previewClass.LOADER)) {
-      maxComment = Math.min(maxComment + COMMENT_VIEW_MAX, commentsData.comments.length);
-      commentsCountCurrent.textContent = maxComment;
-      renderCommentlist(commentsData);
+      var commentList = evt.target.previousElementSibling.children;
+      showComment(commentList);
     }
   };
 
-  var renderComment = function (commentsArr, template) {
+  var getComment = function (commentsArr, template) {
     var commentTemplate = template.cloneNode(true);
+    commentTemplate.classList.add(previewClass.VISUALLY_HIDDEN);
     commentTemplate.querySelector(previewSelector.SOCIAL_PICTURE).src = commentsArr.avatar;
     commentTemplate.querySelector(previewSelector.SOCIAL_TEXT).textContent = commentsArr.message;
     return commentTemplate;
   };
 
-  var renderCommentlist = function () {
+  var hideLoaderButton = function (isHide) {
+    return (isHide) ? loader.classList.add(previewClass.HIDDEN) : loader.classList.remove(previewClass.HIDDEN);
+  };
+
+  var showComment = function (elementList) {
+    var showStep = 0;
+    var currentCount = 0;
+    for (var i = 0; i < elementList.length && showStep < COMMENT_VIEW_MAX; i++) {
+      if (elementList[i].classList.contains(previewClass.VISUALLY_HIDDEN)) {
+        elementList[i].classList.remove(previewClass.VISUALLY_HIDDEN);
+        showStep++;
+      }
+      currentCount++;
+    }
+    renderCommentCount(Math.min(currentCount, elementList.length), elementList.length);
+    hideLoaderButton(elementList.length <= currentCount);
+  };
+
+  var renderCommentlist = function (data) {
     var socialComments = previewBlock.querySelector(previewSelector.SOCIAL_COMMENTS);
     var socialCommentList = socialComments.querySelectorAll(previewSelector.SOCIAL_COMMENT);
     var socialCommentTemplate = socialCommentList[0].cloneNode(true);
-
-    if (commentsData.comments.length <= maxComment) {
-      loader.classList.add(previewClass.HIDDEN);
-    } else {
-      loader.classList.remove(previewClass.HIDDEN);
-    }
-
     window.util.removeChildren(socialComments, socialCommentList);
 
     var fragmentComments = document.createDocumentFragment();
-    for (var k = 0; k < maxComment; k++) {
-      var newComment = renderComment(commentsData.comments[k], socialCommentTemplate);
+    for (var k = 0; k < data.comments.length; k++) {
+      var newComment = getComment(data.comments[k], socialCommentTemplate);
       fragmentComments.appendChild(newComment);
     }
     socialComments.appendChild(fragmentComments);
+    return socialComments;
   };
 
-  var setPreview = function () {
-    previewBlock.querySelector(previewSelector.BIG_IMG).src = commentsData.url;
-    previewBlock.querySelector(previewSelector.LIKES_COUNT).textContent = commentsData.likes;
-    previewBlock.querySelector(previewSelector.SOCIAL_CAPTION).textContent = commentsData.description;
-    commentsCountCurrent.textContent = maxComment;
-    commentsCountFull.textContent = commentsData.comments.length;
+  var setPreview = function (data) {
+    previewBlock.querySelector(previewSelector.BIG_IMG).src = data.url;
+    previewBlock.querySelector(previewSelector.LIKES_COUNT).textContent = data.likes;
+    previewBlock.querySelector(previewSelector.SOCIAL_CAPTION).textContent = data.description;
 
-    renderCommentlist();
+    var commentList = renderCommentlist(data);
+    showComment(commentList.children);
   };
 
   window.preview = {
